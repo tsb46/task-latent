49c787dc60b125f2ed9e1b89ca9bcf8504861d1d
tsb46 <taylor.s.bolt@gmail.com>
2026-08-02 21:59:01 -0400

    move mask to module level constants

--- task_latent/constants.py
+++ task_latent/constants.py
@@ -4,3 +4,6 @@ Constants for the task_latent repo.
 
 # data directory for IBC dataset
 IBC_DATA_DIR = "data/ibc"
+
+# 3mm MNI152 Brain mask
+MASK = "templates/MNI152_3mm_brain_mask_ibc.nii.gz"
--- task_latent/preprocess/constants.py
+++ task_latent/preprocess/constants.py
@@ -7,6 +7,3 @@ HIGHPASS = 0.01
 
 # Full width at half maximum for Gaussian smoothing
 FWHM = 4
-
-# 3mm brain mask for IBC dataset in MNI152 space
-MASK = "templates/MNI152_3mm_brain_mask_ibc.nii.gz"
